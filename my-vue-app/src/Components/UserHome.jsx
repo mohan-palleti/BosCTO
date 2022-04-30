1ae3513605c04c5a1d2c86bb109d013c7fbaf636
mohan_palleti <palletimohanbabu23@gmail.com>
2022-04-30 20:27:41 +0530

    user page update

--- my-vue-app/src/Components/UserHome.jsx
+++ my-vue-app/src/Components/UserHome.jsx
@@ -1,33 +1,47 @@
 import React, { useEffect, useRef, useState } from "react";
 import Navbar from "./Navbar";
-
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 function UserHome() {
-  const Banner_Pictures = [
+  const SliderData = [
     "https://www.provimi.in/image/1432095109188/pet-page-banner-image.jpg",
     "https://www.betterwholesaling.com/wp-content/uploads/2019/06/Pet-Care.jpg",
     "https://www.redcross.org/on/demandware.static/-/Sites-RedCross-Library/default/dw8bc53abf/images/Coronavirus/2020.05.12-Caring-for-Pets-During-COVID-19-850x450.jpg",
   ];
-  const [banner_img, setBannerimg] = useState(
-    "https://www.redcross.org/on/demandware.static/-/Sites-RedCross-Library/default/dw8bc53abf/images/Coronavirus/2020.05.12-Caring-for-Pets-During-COVID-19-850x450.jpg"
-  );
-  let count = useRef(0);
-  console.log(count);
-  useEffect(() => {
-    setInterval(() => {
-      if (count.current < 3) {
-        setBannerimg((prev) => {
-          count.current += 1;
-          return Banner_Pictures[count.current];
-        });
-      }
-    }, 2000);
-  }, []);
+  const [current, setCurrent] = useState(0);
+  const length = SliderData.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(SliderData) || SliderData.length <= 0) {
+    return null;
+  }
 
   return (
-    <div>
+    <>
       <Navbar />
-      <img src={banner_img} width="100%" alt="banners" />
-    </div>
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+        {SliderData.map((slide, index) => {
+          return (
+            <div
+              className={index === current ? "slide active" : "slide"}
+              key={index}
+            >
+              {index === current && (
+                <img src={slide} alt="travel image" className="image" />
+              )}
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
 }
 
